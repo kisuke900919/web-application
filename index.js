bb1fd40d780dfd89076bd20033f626779c63296c
kisuke900919 <86058479+kisuke900919@users.noreply.github.com>
2021-07-08 17:39:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,8 +49,10 @@ app.use(
   authorizationParams: {
    response_type: "code id_token",
    audience: "https://expenses-api",
+     // 👇 add this 👇
+   scope: "openid profile email read:reports",
+   // 👆 add this 👆
   },
-  // 👆 add this 👆
    })
 );
 
